71ba5206a50011e150aff3679cbe206e6cca7891
Omer Shamir <omers100000@gmail.com>
2026-03-16 04:04:01 +0200

    fourth commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
-public class Main {
-    static public void main(String[] args) {
+//import libraries
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
+public class Main {
+    static public void main(String[] args) throws Exception {
+        //create the url connection
+        URL url = new URL("https://query1.finance.yahoo.com/v8/finance/chart/NVDA");
+
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+
+        conn.setRequestProperty("User-Agent", "Mozilla/5.0");
+
+        //bufferread the stocks
+        BufferedReader read = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+        String line;
+
+        while ((line = read.readLine()) != null) {
+            System.out.println(line);
+        }
+
+        read.close();
     }
 }
