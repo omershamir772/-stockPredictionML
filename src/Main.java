3ad2a184099be426aeca553fe099b203ce91a74d
Omer Shamir <omers100000@gmail.com>
2026-03-16 17:32:42 +0200

    start pulling data

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,12 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import org.json.JSONObject;
 
 public class Main {
     static public void main(String[] args) throws Exception {
         //create the url connection
-        URL url = new URL("https://query1.finance.yahoo.com/v8/finance/chart/NVDA?range=3m&interval=1d");
+        URL url = new URL("https://query1.finance.yahoo.com/v8/finance/chart/NVDA?range=1y&interval=1d");
 
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         conn.setRequestMethod("GET");
@@ -18,10 +19,25 @@ public class Main {
         BufferedReader read = new BufferedReader(new InputStreamReader(conn.getInputStream()));
 
         String line;
+        String JsonStock = "";
 
         while ((line = read.readLine()) != null) {
-            System.out.println(line);
+            JsonStock = line;
         }
+
+        try {
+            JSONObject jsonObject =  new JSONObject(JsonStock);
+            JSONObject chart = jsonObject.getJSONObject("chart");
+            System.out.println(chart);
+            //int timestamp = jsonObject.getInt("timestamp");
+            //System.out.println(timestamp);
+
+
+        } catch (org.json.JSONException e) {
+            e.printStackTrace();
+        }
+
+
 
         read.close();
     }
