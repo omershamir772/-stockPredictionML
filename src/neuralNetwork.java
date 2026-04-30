4270a64a0ddad1b0f9b1119a9b681e33cbff8fd8
omershamir772 <omers100000@gmail.com>
2026-04-30 10:43:24 +0300

    start all over again

--- src/neuralNetwork.java
+++ src/neuralNetwork.java
@@ -1,9 +1,1 @@
 
-//create a public class neuralNetwork
-public class neuralNetwork {
-
-    //create the function that will be used for the forward propagation algorithm
-    public static void forwardPropagation() {
-
-    }
-}
